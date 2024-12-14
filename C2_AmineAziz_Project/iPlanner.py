a3cfec2dae8d343aa23ab8d87d089bdc06502f68
AmineAziz <aa.amineaziz@gmail.com>
2024-12-14 10:52:32 +0000

    corrected the login

--- C2_AmineAziz_Project/iPlanner.py
+++ C2_AmineAziz_Project/iPlanner.py
@@ -5,33 +5,39 @@ app = Flask(__name__)
 app.secret_key = 'x1x2x3x4'
 
 def TaskDB_conn(): 
-conn = sqlite3.connect('TaskListDB.db')
-conn.row_f = sqlite3.Row
-return conn
-# conn = sqlite3.connect("TaskListDB.db")
-# cursor = connection.cursor()
-# cursor.execute("""
-# CREATE TABLE IF NOT EXISTS users (
-    # id INTEGER PRIMARY KEY AUTOINCREMENT,
-    # name TEXT NOT NULL,
-    # age INTEGER
-# )
-# """)
-# cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", ("Alice", 30))
-# cursor.execute("SELECT * FROM users")
-# rows = cursor.fetchall()
-# for row in rows:
-    # print(row)
-# connection.commit()
-# connection.close()
+    conn = sqlite3.connect('TaskListDB.db', timeout=10.0)
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/')
+@app.route('/homepage', methods=['GET', 'POST'])
 def index():
+    if 'username' in session:
+        conn = sqlite3.connect('TaskListDB.db', timeout=10.0)
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM Tasks')
+        tasks = cursor.fetchall()
+        conn.close()
+        return redirect(url_for('iPlanner', tasks=tasks))
     return render_template('homepage.html')
     
 @app.route('/', methods=['GET', 'POST'])
 def login():
+    # if request.method == 'POST':
+        # username = request.form['username']
+        # password = request.form['password']
+        # conn = TaskDB_conn()
+        # cursor = conn.cursor()
+        # cursor.execute('SELECT * FROM Users WHERE username = ? AND password = ?', (username, password))
+        # if not cursor.fetchone():
+            # return render_template('homepage.html')
+        # else:
+            # return render_template('iPlanner.html', name = username)
+    # else:
+        # request.method == 'GET':
+        # return render_template('homepage.html')
+    error = None    
     if request.method == 'POST':
-        error = None
         username = request.form['username']
         password = request.form['password']
         conn = TaskDB_conn()
@@ -40,28 +46,51 @@ def login():
         user = cursor.fetchone()
         conn.close()
         
-        if user:
-            session['username']= user['username']
+        if user and username == user['username'] and password == user['password']:
+        # session['username']== user['username']:
             resp = make_response(redirect(url_for('iPlanner')))
+            # make_response(redirect(url_for('iPlanner')))
             resp.set_cookie('user', username, max_age=60*60*24)
             return resp
         else:
-            flash('wrong username or password')
-        # if username == 'admin' and password == 'admin':
-            
+             error='invalid'
+             flash(error)# flash('wrong')
+            # #render_template('homepage.html',error=error),error=error
     return render_template('homepage.html',error=error)
 
 @app.route('/iPlanner', methods=['GET', 'POST'])
 def iPlanner():
-
     user = request.cookies.get('user')
+    task = request.form.get('ListTask')
     if not user:
         return redirect(url_for('login'))
+        
     if request.method == 'POST':
-        text = request.form.get('ListTask')
-        return render_template('iPlanner.html', user=user, text=text)
-    return render_template('iPlanner.html')
-
+        if task:
+            conn = TaskDB_conn()
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO Tasks (content) VALUES (?)', (task,))
+            conn.commit()
+            conn.close()
+    conn = TaskDB_conn()
+    cursor = conn.cursor()
+    cursor.execute('SELECT id, content FROM Tasks ORDER BY id DESC;')
+    contents = cursor.fetchall()
+    conn.close()
+    
+    return render_template('iPlanner.html', user=user, contents=contents)
+    
+@app.route('/iplanner', methods=['GET'])
+def deleteTask():
+    conn = TaskDB_conn()
+    cursor = conn.cursor()
+    
+    if request.method == 'GET':
+        theId = request.form.get('ListTask')
+        cursor.execute('DELETE FROM Tasks WHERE id = ?', (theId,))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('iPlanner.html'))
 if __name__ == '__main__':
-init_db()
+    # init_db()
     app.run(debug=True, port=5000)
